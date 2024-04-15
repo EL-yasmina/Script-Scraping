5abd0ca20b8f4a8572d87b78353ac9ffdfa07914
EL-Yasmina <yasminataif99@gmail.com>
2024-04-15 12:14:02 +0200

    modification de class

--- scraping.py
+++ scraping.py
@@ -3,15 +3,15 @@ from bs4 import BeautifulSoup    # import la bibliothèque bs4
 
 url = "https://meteofrance.com/previsions-meteo-france/paris/75000"   
 response = requests.get(url)   # envoi la requête
-
+#print(response)
 content = response.content
 soup = BeautifulSoup(content, 'html.parser')  # utilisez 'html.parser' au lieu de 'html'
-
-temperature = soup.find( 'strong', class_='temp').text  # utilisez .text pour obtenir le texte et .strip() pour enlever les espaces
+print(soup)
+temperature = soup.find("ul",class_="slider_item single")
 #ressenti = soup.find('span', class_='ressenti')
 #etat_ciel = soup.find('span', class_='etat-ciel')
 
-print("Météo à Paris: ")
-print("Température :"+ temperature) 
+
+print(f"Temperature in Paris: {temperature}°C") 
 #print("ressenti: " + ressenti)
 #print("etat_ciel: " + etat_ciel)
